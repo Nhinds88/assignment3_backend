9e0cca0e1356f94de4a42b7cb9b2a53ef684e035
Nhinds88 <nhinds@csumb.edu>
2020-07-14 15:17:09 -0700

    initial

--- src/main/java/com/cst438/assignment3_backend/Assignment3BackendApplication.java
+++ src/main/java/com/cst438/assignment3_backend/Assignment3BackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Assignment3BackendApplication {
 
+	// will add rabbitmq
 	public static void main(String[] args) {
 		SpringApplication.run(Assignment3BackendApplication.class, args);
 	}
