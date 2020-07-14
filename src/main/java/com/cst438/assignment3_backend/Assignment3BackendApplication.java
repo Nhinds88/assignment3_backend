58fad4dc63bcb09e39db9456e66873e4494825d3
Nhinds88 <nhinds@csumb.edu>
2020-07-14 15:14:47 -0700

    initial

--- src/main/java/com/cst438/assignment3_backend/Assignment3BackendApplication.java
+++ src/main/java/com/cst438/assignment3_backend/Assignment3BackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Assignment3BackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Assignment3BackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment3BackendApplication.class, args);
+	}
 
 }
--- src/test/java/com/cst438/assignment3_backend/Assignment3BackendApplicationTests.java
+++ src/test/java/com/cst438/assignment3_backend/Assignment3BackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Assignment3BackendApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
